242d58b426b0e6c0b0b0feb35917f9ef2c0a843e
김상준 <sangjun.kim@ysmcorp.com>
2016-10-25 17:35:07 +0900

    소스 적용.
    Former-commit-id: 31809d0a2c2dbb707d0f18e2497c5bfeba58188f

--- src/test/java/daon/analysis/ko/TestDictionary.java
+++ src/test/java/daon/analysis/ko/TestDictionary.java
@@ -96,10 +96,12 @@ public class TestDictionary {
 	
 //	@Ignore
 	@Test 
-	public void cAnalyzeKeywordTest() throws IOException{
+	public void analyzeKeywordTest() throws IOException{
 
 		List<String> exampleTexts = new ArrayList<String>();
 		
+		exampleTexts.add("그러자 그는 내게 진러미의 규칙을 가르쳐주었다.");
+		exampleTexts.add("그러자그는내게진러미의규칙을가르쳐주었다.");
 		exampleTexts.add("k2등산화 나이키k5 audi사나이 신발");
 		exampleTexts.add("123,445원");
 		exampleTexts.add("아버지가방에들어가신다");
@@ -109,96 +111,12 @@ public class TestDictionary {
 		exampleTexts.add("형태소 분석기의 적용 분야에 따라 공백이 포함된 고유명사");
 		exampleTexts.add("사람이사랑을할때밥먹어야지");
 		exampleTexts.add("전세계 abc최고가");
+		
+		DaonAnalyzer analyzer = new DaonAnalyzer();
+		analyzer.setDictionary(kkmDic);
+		
 		for(String text : exampleTexts){
-			//띄어쓰기 단위 분리
-			
-			//사전 추출 단어
-			
-			//숫자, 영문, 특수기호 분리
-			
-			//원본 문자
-			char[] texts = text.toCharArray();
-			
-			//총 길이
-			int textLength = text.length();
-	
-			//기분석 사전 매칭 정보 가져오기
-			Map<Integer, List<Term>> lookupResults = kkmDic.lookup(texts, 0, textLength);
-			
-			logger.info("text : {}", text);
-			
-	//		우리말은 제2 유형인 SOV에 속한다. 따라서 국어의 기본 어순은 ‘주어+목적어+서술어’이다.
-			// 1순위) NN 맨 앞 + NN + (J) + V + (E)
-			// 2순위) M 맨 앞 + V + (E)
-			// 3순위) V 맨 앞
-			
-			//결과
-			ResultTerms results = new ResultTerms(lookupResults);
-			
-			//
-			for(int idx=0; idx<textLength;){
-				
-				//idx에 해당하는 기분석 사전 결과 가져오기
-				List<Term> currentTerms = lookupResults.get(idx);
-				
-//				System.out.println(idx + " - " + texts[idx] + " : " + currentTerms);
-	
-				//이전 추출 term 가져오기 
-				Term prevTerm = results.getPrevTerm();
-				
-				//이전 추출 term과 현재 추추 term이 존재
-				if(prevTerm != null && currentTerms != null){
-					//이전 추출 term 이 체언(명사)인 경우 뒤에 조사/어미 여부를 체크
-					if(isNoun(prevTerm)){
-						
-						Term t = firstTerm(currentTerms);
-						if(isJosa(t)){
-	
-							results.add(t);
-							
-							idx += t.getLength();
-							
-							continue;
-						}
-					}
-					
-					//용언(동사) 인 경우 어미 여부 체크 ?
-	//				if(isVerb(currentTerm)){
-	//					
-	//					Term t = firstTerm(terms);
-	//					if(isEomi(t)){
-	//
-	//						result.add(t);
-	//						idx++;
-	//						
-	//						continue;
-	//					}
-	//				}
-				}
-				
-				//현재 기분석 term이 존재하는 경우
-				if(currentTerms != null){
-					//좌 -> 우 최장일치법 or 최적 수치 사용?
-					//마지막이 제일 긴 term 
-					Term lastTerm = lastTerm(currentTerms);
-					int length = lastTerm.getLength();
-					
-					results.add(lastTerm);
-					idx += length;
-				}
-				//미분석 term 처리
-				else{
-					Term unkownTerm = makeUnkownTerm(idx, texts, textLength, lookupResults);
-	
-					int length = unkownTerm.getLength();
-	
-					results.add(unkownTerm);
-					idx += length;
-				}
-				
-	//			System.out.println(texts[idx]);
-			}
-			
+			ResultTerms results = analyzer.analyze(text);
 			
 			System.out.println("################ results #################");
 			for(Term t : results.getResults()){
@@ -208,106 +126,13 @@ public class TestDictionary {
 		
 		}
 		
-		try{
-			Thread.sleep(1000000000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally{
-			
-		}
-	}
-
-	
-	/**
-	 * 미분석 어절 구성
-	 * 
-	 * TODO 영문, 숫자, 한글 타입 구분
-	 * TODO 공백 문자 처리
-	 * 
-	 * @param idx
-	 * @param texts
-	 * @param textLength
-	 * @param lookupResults
-	 * @return
-	 */
-	private Term makeUnkownTerm(int idx, char[] texts, int textLength, Map<Integer, List<Term>> lookupResults) {
-		//기분석 결과에 없는 경우 다음 음절 체크
-		int startIdx = idx;
-		int endIdx = startIdx;
-		
-		while(endIdx < textLength){
-			endIdx++;
-			List<Term> terms = lookupResults.get(endIdx);
-			
-			if(terms != null){
-				break;
-			}
-		}
-
-		int length = (endIdx - startIdx);
-		
-		String unkownWord = new String(texts, startIdx, length);
-		
-		Keyword word = new Keyword(unkownWord, "UNKNOWN");
-		Term unknowTerm = new Term(word, startIdx, length);
-		
-		return unknowTerm;
-	}
-	
-	private Term firstTerm(List<Term> terms) {
-		if(terms == null){
-			return null;
-		}
-		
-		Term t = terms.get(0);
-		return t;
-	}
-	
-	private Term lastTerm(List<Term> terms) {
-		if(terms == null){
-			return null;
-		}
-		
-		int lastIdx = terms.size() -1;
-		Term t = terms.get(lastIdx);
-		
-		return t;
-	}
-
-	private boolean isNoun(Term t){
-		
-		return isStartWith(t,  "N");
-	}
-
-	private boolean isJosa(Term t){
-		
-		return isStartWith(t,  "J");
-	}
-
-
-	private boolean isVerb(Term t){
-		
-		return isStartWith(t,  "V");
-	}
-
-	private boolean isEomi(Term t){
-		
-		return isStartWith(t,  "E");
-	}
-	
-	private boolean isStartWith(Term t, String startWith){
-		boolean is = false;
-		
-		if(t != null && t.getKeyword() != null){
-			
-			for(String attr : t.getKeyword().getAttr()){
-				if(attr.startsWith(startWith)){
-					return true;
-				}
-			}
-		}
-		
-		return is;
+//		try{
+//			Thread.sleep(1000000000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}finally{
+//			
+//		}
 	}
 }
