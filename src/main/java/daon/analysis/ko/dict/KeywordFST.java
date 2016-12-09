7eb2cf36da3eb0beccd9c87a499be4c9fc821691
김상준 <sangjun.kim@ysmcorp.com>
2016-12-09 18:12:59 +0900

    원복.
    Former-commit-id: 25a92d43ad606f07f2c551fff14d3f6b5ac9925d

--- src/main/java/daon/analysis/ko/dict/KeywordFST.java
+++ src/main/java/daon/analysis/ko/dict/KeywordFST.java
@@ -19,6 +19,7 @@ package daon.analysis.ko.dict;
 
 import java.io.IOException;
 
+import org.apache.lucene.util.IntsRef;
 import org.apache.lucene.util.fst.FST;
 import org.apache.lucene.util.fst.FST.Arc;
 
@@ -36,16 +37,16 @@ public final class KeywordFST {
 	//한글 종료 문자
 	private final static int end = Utils.KOR_END;
 	
-	private final FST<Long> fst;
+	private final FST<IntsRef> fst;
 
 	// depending upon fasterButMoreRam, we cache root arcs for either
 	// korean (0xAC00-0xD7A3) type="KOREAN"; // 44032, 55203 = 11171
 	private final int cacheCeiling;
-	private final FST.Arc<Long> rootCache[];
+	private final FST.Arc<IntsRef> rootCache[];
 
-	public final Long NO_OUTPUT;
+	public final IntsRef NO_OUTPUT;
 
-	public KeywordFST(FST<Long> fst) throws IOException {
+	public KeywordFST(FST<IntsRef> fst) throws IOException {
 		this.fst = fst;
 		this.cacheCeiling = end; // 한글 캐싱.
 		NO_OUTPUT = fst.outputs.getNoOutput();
@@ -53,27 +54,27 @@ public final class KeywordFST {
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private FST.Arc<Long>[] cacheRootArcs() throws IOException {
-		FST.Arc<Long> rootCache[] = new FST.Arc[1 + (end - start)];
-		FST.Arc<Long> firstArc = new FST.Arc<>();
+	private FST.Arc<IntsRef>[] cacheRootArcs() throws IOException {
+		FST.Arc<IntsRef> rootCache[] = new FST.Arc[1 + (end - start)];
+		FST.Arc<IntsRef> firstArc = new FST.Arc<>();
 		fst.getFirstArc(firstArc);
-		FST.Arc<Long> arc = new FST.Arc<>();
+		FST.Arc<IntsRef> arc = new FST.Arc<>();
 		final FST.BytesReader fstReader = fst.getBytesReader();
 		// TODO: jump to 3040, readNextRealArc to ceiling? (just be careful we
 		// don't add bugs)
 		for (int i = 0; i < rootCache.length; i++) {
 			if (fst.findTargetArc(start + i, firstArc, arc, fstReader) != null) {
-				rootCache[i] = new FST.Arc<Long>().copyFrom(arc);
+				rootCache[i] = new FST.Arc<IntsRef>().copyFrom(arc);
 			}
 		}
 		return rootCache;
 	}
 
-	public FST.Arc<Long> findTargetArc(int ch, FST.Arc<Long> follow, FST.Arc<Long> arc, boolean useCache,
+	public FST.Arc<IntsRef> findTargetArc(int ch, FST.Arc<IntsRef> follow, FST.Arc<IntsRef> arc, boolean useCache,
 			FST.BytesReader fstReader) throws IOException {
 		if (useCache && ch >= start && ch <= end) {
 			assert ch != FST.END_LABEL;
-			final Arc<Long> result = rootCache[ch - start];
+			final Arc<IntsRef> result = rootCache[ch - start];
 			if (result == null) {
 				return null;
 			} else {
@@ -86,7 +87,7 @@ public final class KeywordFST {
 		}
 	}
 
-	public Arc<Long> getFirstArc(FST.Arc<Long> arc) {
+	public Arc<IntsRef> getFirstArc(FST.Arc<IntsRef> arc) {
 		return fst.getFirstArc(arc);
 	}
 
@@ -97,7 +98,7 @@ public final class KeywordFST {
 	/**
 	 * @lucene.internal for testing only
 	 */
-	FST<Long> getInternalFST() {
+	FST<IntsRef> getInternalFST() {
 		return fst;
 	}
 }
