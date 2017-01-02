5b9fa9052551e72522bc7d67efbeadad9425ac3a
김상준 <sangjun.kim@ysmcorp.com>
2017-01-02 17:53:53 +0900

    처리 로직 수정.
    Former-commit-id: 544e9672e9863941f49987790fd9ab0b66afa5be

--- src/main/java/daon/analysis/ko/tag/Tag.java
+++ src/main/java/daon/analysis/ko/tag/Tag.java
@@ -2,10 +2,12 @@ package daon.analysis.ko.tag;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import daon.analysis.ko.dict.config.Config.POSTag;
+import daon.analysis.ko.util.Utils;
 
 public class Tag {
 	
@@ -14,8 +16,25 @@ public class Tag {
 	private Map<String,Long> tagBits = new HashMap<String,Long>();
 	
 	public Tag(Map<String,Long> tagBits) {
-		
 		this.tagBits = tagBits;
 	}
 	
+	public boolean isValid(String tagKey, POSTag tag){
+		boolean isValid = false;
+		
+		Long bits = tagBits.get(tagKey);
+		
+		
+		if(bits == null){
+			return isValid;
+		}
+		
+		long result = bits & tag.getBit();
+		
+		if(result > 0){
+			isValid = true;
+		}
+		
+		return isValid;
+	}
 }
--- src/test/java/daon/analysis/ko/TestStep1.java
+++ src/test/java/daon/analysis/ko/TestStep1.java
@@ -71,6 +71,8 @@ public class TestStep1 {
 		
 			DaonAnalyzer analyzer = new DaonAnalyzer(dic);
 			analyzer.setTag(tag);
+			
+			
 		
 			ResultTerms results = analyzer.analyze(text);
 			
