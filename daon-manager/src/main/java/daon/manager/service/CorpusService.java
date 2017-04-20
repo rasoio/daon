2330679905db9590f182bc85291a1e27c8962511
김상준 <sangjun.kim@ysmcorp.com>
2017-04-20 18:09:02 +0900

    형태소 분석 web 말뭉치 결과 추가
    Former-commit-id: 76c5d987087278b7a4e888fe458a213dfb999260

--- daon-manager/src/main/java/daon/manager/service/CorpusService.java
+++ daon-manager/src/main/java/daon/manager/service/CorpusService.java
@@ -1,30 +1,21 @@
 package daon.manager.service;
 
-import daon.analysis.ko.model.CandidateTerm;
+import daon.analysis.ko.model.MatchInfo;
 import daon.manager.model.CorpusParams;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.lucene.search.Explanation;
 import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.lucene.search.function.CombineFunction;
-import org.elasticsearch.common.lucene.search.function.FiltersFunctionScoreQuery;
 import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.InnerHitBuilder;
-import org.elasticsearch.index.query.NestedQueryBuilder;
-import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
-import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import static org.elasticsearch.index.query.QueryBuilders.*;
-import static org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders.fieldValueFactorFunction;
 
 /**
  * Created by mac on 2017. 3. 9..
@@ -41,13 +32,55 @@ public class CorpusService {
 
 		BoolQueryBuilder boolQueryBuilder = boolQuery();
 
-		List<String> seqs = params.getSeq();
+		List<Integer> seqs = params.getSeq();
 
 		if(seqs != null){
-			for(String seq : seqs) {
+			for(Integer seq : seqs) {
 				boolQueryBuilder.must(matchQuery("word_seqs", seq));
 			}
 		}
+
+		MatchInfo matchInfo = params.getMatchInfo();
+
+		if(matchInfo != null){
+			MatchInfo.MatchType type = matchInfo.getType();
+
+			switch (type) {
+				case DICTIONARY:
+					int[] matchSeqs = matchInfo.getSeqs();
+
+					for(Integer seq : matchSeqs) {
+						boolQueryBuilder.must(matchQuery("word_seqs", seq));
+					}
+
+					break;
+				case PREV_CONNECTION:
+
+					String prevFieldName = "eojeols.morphemes.p_inner_seq";
+
+					if(matchInfo.isOuter()){
+						prevFieldName = "eojeols.morphemes.p_outer_seq";
+					}
+
+					boolQueryBuilder.must(nestedQuery("eojeols.morphemes",
+						boolQuery()
+								.must(matchQuery(prevFieldName, matchInfo.getPrevSeq()))
+								.must(matchQuery("eojeols.morphemes.seq", matchInfo.getSeq()))
+					, ScoreMode.None));
+
+					break;
+				case NEXT_CONNECTION:
+
+					boolQueryBuilder.must(nestedQuery("eojeols.morphemes",
+						boolQuery()
+								.must(matchQuery("eojeols.morphemes.seq", matchInfo.getSeq()))
+								.must(matchQuery("eojeols.morphemes.n_inner_seq", matchInfo.getNextSeq()))
+					, ScoreMode.None));
+
+					break;
+			}
+		}
+
 
 		SearchRequestBuilder searchRequestBuilder = esClient.prepareSearch("corpus")
 				.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
@@ -69,34 +102,6 @@ public class CorpusService {
 		SearchResponse response = searchRequestBuilder
 				.execute()
 				.actionGet();
-
-//		List<Content> results = contentsResponse.getContent();
-//
-//		response.getHits().forEach( hit -> {
-//			Map<String, Object> source = hit.getSource();
-//			source.put("score", hit.getScore());
-//
-//			if(filter.isDebug()) {
-//				// Debug mode
-//				Explanation explanation = hit.getExplanation();
-//				source.put("explanation", explanation);
-//			}
-//
-//			results.add(new Content(source));
-//		});
-//
-//		// Debug mode
-//		if(filter.isDebug()) {
-//			log.info("query => \n{}", searchRequestBuilder);
-//			contentsResponse.setProfileResults(response.getProfileResults());
-//		}
-//
-//		contentsResponse.setNumberOfElements(response.getHits().hits().length);
-//		contentsResponse.setTotalElements(response.getHits().totalHits());
-//		contentsResponse.calculateTotalPages();
-//
-//		contentsResponse.setTerms(analyzedTerms);
-
 
 
 		return response;
